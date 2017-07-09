68c7a602dfc3e72989ca4e7caf4721d9bd9c43de
David Cristofaro <david@dtcristo.com>
2017-07-09 23:54:45 +1000

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+#------------------------------
+# chruby, ruby version switcher
+#------------------------------
+set -gx CHRUBY_ROOT /usr
+
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
