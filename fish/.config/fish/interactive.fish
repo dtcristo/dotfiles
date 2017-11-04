0a7d6817eb011b7fc8bd2e3b5e7c1e08793f85d3
David Cristofaro <david@dtcristo.com>
2017-11-04 20:54:53 +1100

    Re-add abbr's

--- fish/.config/fish/interactive.fish
+++ fish/.config/fish/interactive.fish
@@ -4,3 +4,10 @@ source $HOME/.config/fish/interactive.d/colors.fish
 # Base16 shell colors
 eval sh $HOME/.config/base16-shell/scripts/base16-solarflare.sh
 source $HOME/.config/base16-shell/profile_helper.fish
+
+# Abbreviations
+set -g fish_user_abbreviations
+abbr -a be 'bundle exec'
+abbr -a dig 'drill'
+abbr -a to_staging 'git checkout staging; and git merge development; and git push; and git checkout development'
+abbr -a to_master 'git checkout master; and git merge staging; and git push; and git checkout development'
