8451da0d64c6637fa3abe81ae8ee347dc2049ab7
David Cristofaro <david@dtcristo.com>
2017-11-04 15:14:35 +1100

    Remove debug echos

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,13 +5,11 @@ source $HOME/.config/fish/environment.fish
 # Interactive shell
 #------------------
 if status --is-interactive
-  echo "Interactive fish"
   source $HOME/.config/fish/interactive.fish
 end
 
 # Login shell
 #------------
 if status --is-login
-  echo "Login fish"
   source $HOME/.config/fish/login.fish
 end
