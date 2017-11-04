73fb06eb384e17ccf8642236fb2224b5867b15b5
David Cristofaro <david@dtcristo.com>
2017-11-04 23:13:55 +1100

    Add `fish_mode_prompt`

---
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,23 @@
+# The fish_mode_prompt function is prepended to the prompt
+function fish_mode_prompt --description "Displays the current mode"
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --reverse $color_blue
+                echo ' N '
+            case insert
+                set_color --reverse $color_green
+                echo ' I '
+            case replace_one
+                set_color --reverse $color_red
+                echo ' R '
+            case visual
+                set_color --reverse $color_orange
+                echo ' V '
+        end
+        set_color normal
+        echo -n ' '
+    end
+end
