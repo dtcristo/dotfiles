bf4be4ddd2b0d63affd51ab941ad50a79db99e27
David Cristofaro <david@dtcristo.com>
2017-11-05 23:05:29 +1100

    Changes to vi mode test

--- fish/.config/fish/functions/fish_mode_prompt.fish
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -1,8 +1,7 @@
 # The fish_mode_prompt function is prepended to the prompt
 function fish_mode_prompt --description "Displays the current mode"
     # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+    if using_vi_key_bindings
         switch $fish_bind_mode
             case default
                 set_color --reverse $color_blue
