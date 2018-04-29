e93071743a01d35c7d34404c678ffd64fab3a24b
David Cristofaro <david@dtcristo.com>
2018-04-29 13:48:07 +1000

    Tweaks to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,10 @@
 # chruby, ruby version switcher
 set -gx CHRUBY_ROOT /usr
 if test -f /usr/local/share/chruby/chruby.fish
-  source /usr/local/share/chruby/chruby.fish
+    source /usr/local/share/chruby/chruby.fish
 end
 if test -f /usr/local/share/chruby/auto.fish
-  source /usr/local/share/chruby/auto.fish
+    source /usr/local/share/chruby/auto.fish
 end
 
 # Interactive shell
@@ -12,7 +12,7 @@ end
 if status --is-interactive
     # Base16 shell colors
     if test -f $HOME/.config/base16-shell/scripts/base16-solarflare.sh
-      eval sh $HOME/.config/base16-shell/scripts/base16-solarflare.sh
+        eval sh $HOME/.config/base16-shell/scripts/base16-solarflare.sh
     end
 
     # BUG: Already set as universal variable, should not need to set here
@@ -22,7 +22,6 @@ end
 # Login shell
 #------------
 if status --is-login
-    # neofetch prints system info on login
-    echo
-    neofetch
+    # Clear screen and print system info on login
+    clear; echo; neofetch
 end
